6b6def779698b78c55218e2fbcd5204c7c5e86d4
Djbomba338 <francocardozomato@gmail.com>
2025-06-23 17:53:50 -0300

    disfruta jp

--- js/clases.js
+++ js/clases.js
@@ -1,36 +1,158 @@
 class Sistema {
     constructor (){
+        this.listaCarrera = []
+        this.listaCorredor = []
+        this.listaPatrocinador = []
+        this.listaInscripcion = []
+    }
+    validarNom(nombreNuevo){
+        let validar = false
+        for (let i = 0; i < this.listaCarrera.length ; i++){
+            if(this.listaCarrera[i].nombre == nombreNuevo){
+                validar = true
+                break
+            } 
+        }
+        return validar
+    }
 
+
+    nuevaCarrera(nCarrera){
+        this.listaCarrera.push(nCarrera)
+    } 
+
+
+    validarNomRub(nom , rubro){
+        let validar = false
+        for (let i = 0; i < this.listaPatrocinador.length ; i++){
+            if(this.listaPatrocinador[i].nombre == nom && this.listaPatrocinador[i].rubro == rubro){
+                validar = true
+                break
+            } 
+        }
+        return validar
+    }
+
+
+    actualizarRubro(nom, rubro){
+        for (let i = 0; i < this.listaPatrocinador.length ; i++){
+                if (this.listaPatrocinador[i].nombre == nom && this.listaPatrocinador[i].rubro != rubro){
+                    this.listaPatrocinador[i].rubro = rubro;
+                    return true
+                }
+        } 
+        return false
+    }
+
+
+    nuevoPatrocinador(nPatrocinador){
+        this.listaPatrocinador.push(nPatrocinador)
+    }
+    
+
+    validarCedula(cedula){
+        let validar = false
+        for (let i = 0; i < this.listaCorredor.length ; i++){
+            if(this.listaCorredor[i].cedula == cedula){
+                validar = true
+                break
+            } 
+        }
+        return validar
+    }
+
+    validarFichaMedica(fichaMedica){
+        let validar = false
+        for (let i = 0; i < this.listaCarrera.length ; i++){
+            if(this.listaCarrera[i].fecha > fichaMedica){
+                validar = true
+                break
+            } 
+        }
+        return validar
+    }
+
+
+
+
+    nuevoCorredor(nCorredor){
+        this.listaCorredor.push(nCorredor)
+    }
+
+    encontrarCorredor(cedula){
+        for (let i = 0; i < this.listaCorredor.length ; i++){
+            if(this.listaCorredor[i].cedula == cedula){ 
+                return this.listaCorredor[i]
+            }
+        }
+    }
+
+    nuevaInscripcion(nInscripcion){
+        let carrera = nInscripcion.carrera
+        if(carrera.cuposUsados < carrera.cupos){
+            this.listaInscripcion.push(nInscripcion)
+            carrera.cuposUsados += 1
+            carrera.cupos -= 1
+        } else {
+            alert("No hay cupos disponibles para esta carrera")
+        }
+    }
+
+
+    valdiarNuevaInscripcion(carrera){
+        if(carrera.cuposUsados < carrera.cupos){
+            return true
+        }
     }
 }
+
+
+
 class Carrera {
-    constructor (nombre, edad, fecha, cupos){
+    constructor (nombre, departamento, fecha, cupos){
         this.nombre = nombre;
-        this.edad = edad
+        this.departamento = departamento
         this.fecha = fecha
         this.cupos = cupos 
+        this.cuposUsados = 0
     }
 }
+
+
 class Corredor {
-    constructor (nombre, edad, cedula, nacimiento,tipo){
+    constructor (nombre, edad, cedula, fichaMedica,tipo){
         this.nombre = nombre 
         this.edad = edad
         this.cedula =  cedula 
-        this.nacimiento = nacimiento
+        this.fichaMedica = fichaMedica
         this.tipo =  tipo 
     }
+    toString(){
+        return this.nombre + this.edad + "años CI:" + this.cedula + this.fichaMedica + this.tipo
+    }
 }
+
+
 class Inscripcion {
     constructor (corredor, carrera){
         this.corredor = corredor 
         this.carrera = carrera 
-
+    }
+    toString(){
+        return this.corredor + this.carrera
     }
 }
+
+
 class Patrocinador {
-    constructor(nombre,rubro,carrera){
+    constructor(nombre,rubro,carrera = []){
         this.nombre = nombre 
         this.rubro = rubro 
-        this. carrera = carrera
+        this.carrera = carrera
+    }
+    agregarCarrera(carrera){
+        this.carrera.push(carrera)
     }
 }
+
+
---
+++ js/funciones.js
@@ -0,0 +1,142 @@
+window.addEventListener("load",inicio)
+let sistema = new Sistema()
+function inicio(){
+    document.getElementById("idDatos").addEventListener("click", mostrar)
+    document.getElementById("idEstadisticas").addEventListener("click", mostrar)
+    document.getElementById("agregar_carrera").addEventListener("click", sacarDatosCarrera)
+    document.getElementById("agregar_patrocinador").addEventListener("click", sacarDatosPatrocinador)
+    document.getElementById("agregar_corredor").addEventListener("click", sacarDatosCorredores)
+    document.getElementById("agregar_inscripcion").addEventListener("click", sacarDatosInscripcion)
+}
+
+
+function mostrar(evento){
+    if(evento.target == document.getElementById("idDatos")){
+        document.getElementById("datos").style.display = "block"
+        document.getElementById("estadisticas").style.display = "none"
+    } else {
+        document.getElementById("datos").style.display = "none"
+        document.getElementById("estadisticas").style.display = "block"   
+    }
+}
+
+
+function sacarDatosCarrera (evento){
+    evento.preventDefault()
+    let newNombre = document.getElementById("carreras_nombre").value
+    let newDepartamento = document.getElementById("carreras_departamento").value
+    let newFecha = document.getElementById("agregar_carrera_fecha").value
+    let newCupos = document.getElementById("agregar_carrera_cupo").value
+    if(!sistema.validarNom(newNombre)) {
+        let newArray =  new Carrera(newNombre , newDepartamento , newFecha , newCupos )
+        sistema.nuevaCarrera(newArray)
+        let select = document.getElementById("patrocinadores_carreras");
+        let option = document.createElement("option");
+        option.value = newNombre;
+        option.textContent = newNombre;
+        select.appendChild(option);
+        document.getElementById("carreras_nombre").value = ""
+        document.getElementById("carreras_departamento").selectedIndex = 0
+        document.getElementById("agregar_carrera_fecha").value = ""
+        document.getElementById("agregar_carrera_cupo").value = "30"
+        actualizarSelectCarreras("inscripciones_carreras");
+    }
+}
+
+
+function actualizarSelectCarreras(selectId) {
+    let select = document.getElementById(selectId);
+    select.innerHTML = "";
+    for (let carrera of sistema.listaCarrera) {
+        let option = document.createElement("option");
+        option.value = carrera.nombre;
+        option.textContent = carrera.nombre;
+        select.appendChild(option);
+    }
+}
+
+
+function actualizarSelectCorredor(selectId) {
+    let select = document.getElementById(selectId);
+    select.innerHTML = "";
+    for (let corredor of sistema.listaCorredor) {
+        let option = document.createElement("option");
+        option.value = corredor.nombre ;
+        option.textContent = corredor.nombre + " - " + corredor.cedula;
+        select.appendChild(option);
+    }
+}
+
+
+function sacarDatosPatrocinador(evento){
+    evento.preventDefault()
+    let newNombre = document.getElementById("patrocinadores_nombre").value
+    let newRubro = document.getElementById("patrocinadores_rubro").value
+    let select = document.getElementById("patrocinadores_carreras");
+    let carreraSeleccionada = []
+    for (let i = 0; i < select.options.length; i++) {
+        if (select.options[i].selected) {
+            carreraSeleccionada.push(select.options[i].value);
+        }
+    }
+    if(!sistema.validarNomRub(newNombre, newRubro) && !sistema.actualizarRubro(newNombre, newRubro)) {
+        let newArray =  new Patrocinador(newNombre , newRubro, carreraSeleccionada)
+        sistema.nuevoPatrocinador(newArray)
+        document.getElementById("patrocinadores_nombre").value = ""
+        document.getElementById("patrocinadores_rubro").selectedIndex = 0
+        document.getElementById("patrocinadores_carreras").selectedIndex = 0
+    } else {
+        document.getElementById("patrocinadores_nombre").value = ""
+        document.getElementById("patrocinadores_rubro").selectedIndex = 0
+        document.getElementById("patrocinadores_carreras").selectedIndex = 0
+    }
+}
+
+
+function sacarDatosCorredores(evento){
+    evento.preventDefault()
+    let newNombre = document.getElementById("corredores_nombreCorredor").value
+    let cedula = document.getElementById("corredores_cedula").value
+    let edad = document.getElementById("corredores_edad").value
+    let fecha = document.getElementById("corredores_fecha_vencimiento").value
+    let tipoCorredor = ""
+    if(document.getElementById("corredores_tipo_corredor_elite").checked){
+        tipoCorredor = "Deportista de élite"
+    } else {
+        tipoCorredor = "Deportista Común"
+    }
+    if(!sistema.validarCedula(cedula)) {
+        let newArray =  new Corredor(newNombre , edad , cedula , fecha, tipoCorredor)
+        sistema.nuevoCorredor(newArray)
+        document.getElementById("corredores_nombreCorredor").value = ""
+        document.getElementById("corredores_cedula").value = ""
+        document.getElementById("corredores_edad").value = ""
+        document.getElementById("corredores_fecha_vencimiento").value = ""
+        actualizarSelectCorredor("inscripciones_corredores");
+    } else {
+        alert("Ingrese datos nuevos, ya existe un corredor con esa cédula o ficha médica")
+    }
+}
+
+
+function sacarDatosInscripcion(evento){
+    evento.preventDefault()
+    let selectCorredor = document.getElementById("inscripciones_corredores");
+    let corredorSeleccionado = selectCorredor.cedula;
+    let selectCarrera = document.getElementById("inscripciones_carreras");
+    let carreraSeleccionadaNombre = selectCarrera.value;
+    let carreraSeleccionada ;
+    for (let i = 0; i < sistema.listaCarrera.length; i++) {
+        if (sistema.listaCarrera[i].nombre === carreraSeleccionadaNombre) {
+            carreraSeleccionada = sistema.listaCarrera[i];
+            break;
+        }
+    }
+
+    if(sistema.valdiarNuevaInscripcion(carreraSeleccionada) && !sistema.validarFichaMedica(fecha)) {   
+        let newArray =  new Inscripcion(corredorSeleccionado , carreraSeleccionada)
+        sistema.nuevaInscripcion(newArray)
+    } else {
+        alert("No hay cupos disponibles para esta carrera")
+    }
+}
